bd22cb993973869326b4ee59346b54cfefbaeb03
Andrea Pacino <andrea.pacino87@gmail.com>
2017-11-12 10:41:13 +0100

    refactor: Removed BookShelfChanger component.
    BookShelfChanger component will not be used outside the Book component
    for that reason its content has been included in the parent component.

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BookShelfChanger from './BookShelfChanger'
 import PropTypes from 'prop-types'
 
 const Book = (props) => {
@@ -8,9 +7,18 @@ const Book = (props) => {
   <div className="book">
     <div className="book-top">
       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})`}}></div>
-      <BookShelfChanger
-        currentShelf={props.book.shelf}
-        onBookChangeShelf={(shelf) => props.onBookChangeShelf(shelf) }/>
+      <div className="book-shelf-changer">
+        <select
+          value={props.book.shelf}
+          onChange={ (e) => { props.onBookChangeShelf(e.target.value); } }
+        >
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
     </div>
     <div className="book-title">{props.book.title}</div>
     <div className="book-authors">{props.book.authors}</div>
